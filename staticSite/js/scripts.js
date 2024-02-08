90da630657f1e74ad5af5ef246b26dd51474d8e9
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2024-02-08 18:42:05 -0500

    created react boilerplate and moved old site to ./old

--- staticSite/js/scripts.js
+++
@@ -1,9 +0,0 @@
-function menu() {
-    var x = document.getElementById("myNav")
-    if(x.style.display === 'flex') {
-        x.style.display = none
-    } else {
-        x.style.display = 'flex'
-        x.style.flexDirection = 'column'
-    }
-}
