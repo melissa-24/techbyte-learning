1dd9a8b831f2e0a74f46385deea0fa098b973242
cgbridgewater <cgbridgewater@outlook.com>
2024-02-09 23:27:24 -0800

    Building nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import bee_logo from "../assets/images/tech_byte_learning_logo.png"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div class="page_title">
+                <img src={ bee_logo } alt="TechByte Learning Bee" />
+                <h1>TechByte Learning</h1>
+            </div>
+            <ul class="navbar_menu">
+                <li>
+                <a href="#">Services</a>
+                <ul class="drop menu">
+                    <li><a href="#">Home Page</a></li>
+                    <li><a href="#">Web Development</a></li>
+                    <li><a href="#">Tutoring</a></li>
+                    <li><a href="#">Something Here!!</a></li>
+                </ul>
+                </li>
+                <li><a href="#">News</a>
+                <ul class="drop menu">
+                    <li><a href="#">Next Cohort</a></li>
+                    <li><a href="#">Hot Off The Press</a></li>
+                    <li><a href="#">Something Here!!</a></li>
+                    <li><a href="#">Something Here Too!</a></li>
+                </ul>
+                </li>
+                <li><a href="#">About Us</a>
+                <ul class="drop menu">
+                    <li><a href="#">Meet Our Hive</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Our Discord</a></li>
+                </ul>
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Navbar;
