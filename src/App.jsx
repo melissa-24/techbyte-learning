54379381ed359b57313e02829ef523fb0678d275
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2024-02-10 15:42:01 -0500

    add Courses.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Index from "./views/Index";
 import About from "./views/About";
+import Courses from "./views/Courses";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Index />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/courses" element={<Courses />} />
       </Routes>
       <Footer />
     </>
---
+++ src/views/Courses.jsx
@@ -0,0 +1,9 @@
+const Courses = () => {
+  return (
+    <main>
+      <h1>Our Course Offerings</h1>
+    </main>
+  );
+};
+
+export default Courses;
