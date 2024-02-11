9da0b828cbd39bb80e0ccc808aabb92ccce3e786
cgbridgewater <cgbridgewater@outlook.com>
2024-02-11 12:29:17 -0800

    update style and changed class to className to satisfy DOM errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import bee_logo from "../assets/images/tech_byte_learning_logo.png"
 
 const Navbar = () => {
     return (
-        <nav class="navbar">
-            <div class="page_title">
+        <nav className="navbar">
+            <div className="page_title">
                 <img src={ bee_logo } alt="TechByte Learning Bee" />
                 <h1>TechByte Learning</h1>
             </div>
-            <ul class="navbar_menu">
+            <ul className="navbar_menu">
                 <li>
                 <a href="#">Services</a>
-                <ul class="drop menu">
+                <ul className="drop menu">
                     <li><a href="#">Home Page</a></li>
                     <li><a href="#">Web Development</a></li>
                     <li><a href="#">Tutoring</a></li>
@@ -18,7 +18,7 @@ const Navbar = () => {
                 </ul>
                 </li>
                 <li><a href="#">News</a>
-                <ul class="drop menu">
+                <ul className="drop menu">
                     <li><a href="#">Next Cohort</a></li>
                     <li><a href="#">Hot Off The Press</a></li>
                     <li><a href="#">Something Here!!</a></li>
@@ -26,7 +26,7 @@ const Navbar = () => {
                 </ul>
                 </li>
                 <li><a href="#">About Us</a>
-                <ul class="drop menu">
+                <ul className="drop menu">
                     <li><a href="#">Meet Our Hive</a></li>
                     <li><a href="#">Contact Us</a></li>
                     <li><a href="#">Careers</a></li>
