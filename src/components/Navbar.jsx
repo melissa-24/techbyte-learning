9cb420309587bf9eb13cfb54cf475ad6f98b5381
cgbridgewater <cgbridgewater@outlook.com>
2024-02-12 15:51:35 -0800

    merge

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import bee_logo from "../assets/images/tech_byte_learning_logo.png"
-
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="page_title">
-                <img src={ bee_logo } alt="TechByte Learning Bee" />
-                <h1>TechByte Learning</h1>
-            </div>
-            <ul className="navbar_menu">
-                <li>
-                <a href="#">Services</a>
-                <ul className="drop menu">
-                    <li><a href="#">Home Page</a></li>
-                    <li><a href="#">Web Development</a></li>
-                    <li><a href="#">Tutoring</a></li>
-                    <li><a href="#">Something Here!!</a></li>
-                </ul>
-                </li>
-                <li><a href="#">News</a>
-                <ul className="drop menu">
-                    <li><a href="#">Next Cohort</a></li>
-                    <li><a href="#">Hot Off The Press</a></li>
-                    <li><a href="#">Something Here!!</a></li>
-                    <li><a href="#">Something Here Too!</a></li>
-                </ul>
-                </li>
-                <li><a href="#">About Us</a>
-                <ul className="drop menu">
-                    <li><a href="#">Meet Our Hive</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Careers</a></li>
-                    <li><a href="#">Our Discord</a></li>
-                </ul>
-                </li>
-            </ul>
-        </nav>
-    )
-};
-
-export default Navbar;
-=======
 import { Link } from "react-router-dom";
 
 import bee_logo from "../assets/images/tech_byte_learning_logo.png"
@@ -84,5 +41,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
->>>>>>> dev
