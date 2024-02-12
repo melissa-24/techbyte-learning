1908dd583e9b2462085f5a05eaab7aa641bbe6e3
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2024-02-12 13:06:07 -0500

    fixed small console error regarding class vs className and for vs htmlFor

--- src/components/Cookies.jsx
+++ src/components/Cookies.jsx
@@ -3,18 +3,18 @@ const Cookies = () => {
     return (
         <div>
             <input type="checkbox" id="showblock"/>
-            <div class="slideout">
-                <label class="slideout_tab" for="showblock" title="Help">
-                <span class="tab_text">
+            <div className="slideout">
+                <label className="slideout_tab" htmlFor="showblock" title="Help">
+                <span className="tab_text">
                     <span>Cookie Info</span>
                 </span>
                 </label>
-                <div class="slideout_content">
-                <div class="push_right">
-                    <p class="pst">Psst....<img class="cookie_img" src="https://lordicon.com/icons/wired/flat/2354-cookies.svg"/></p>
-                    <p class="cookie">We use Cookies to offer you a better browsing experience, analyse site traffic and personalize content.
+                <div className="slideout_content">
+                <div className="push_right">
+                    <p className="pst">Psst....<img className="cookie_img" src="https://lordicon.com/icons/wired/flat/2354-cookies.svg"/></p>
+                    <p className="cookie">We use Cookies to offer you a better browsing experience, analyse site traffic and personalize content.
                     </p>
-                    <p class="cookie">By continuing to browse TechByte Learning you will consent to our use of cookies.</p>
+                    <p className="cookie">By continuing to browse TechByte Learning you will consent to our use of cookies.</p>
                 </div>
                 </div>
             </div>
