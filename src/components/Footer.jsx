dca0e63d6b483733f0e9b8c9776685224c35e420
cgbridgewater <cgbridgewater@outlook.com>
2024-02-13 21:35:48 -0800

    updating footer to have new logo and save the bees link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import vet_owned from "../assets/images/vet_owned.png"
-import woman_owned from "../assets/images/woman_owned.png"
+import woman_owned from "../assets/images/woman_owned2.png"
+// import woman_owned from "../assets/images/woman_owned.png"
 
 const Footer = () => {
   return (
     <footer>
-      <p>#SaveTheBees</p>
-      <div>
-        <img src={vet_owned} alt="vet owned" />
-        <img src={woman_owned} alt="vet owned" />
+      <a href="https://savethebees.com/" target="_blank">#SaveTheBees</a>
+      <div className="footer_images">
+        <img className="footer_image_one" src={vet_owned} alt="vet owned" />
+        <img className="footer_image_two" src={woman_owned} alt="vet owned" />
       </div>
     </footer>
   )
